83ac9b3ae82ace5009ffd52c61bfa2baddfa12f1
j3c118156 <72444469+j3c118156@users.noreply.github.com>
2020-10-12 21:32:43 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var marker = document.querySelector('.marker');
+
+var item = document.querySelectorAll('.content a');
+
+function indicator(e){
+    marker.style.left = e.offsetLeft + "px";
+    marker.style.width = e.offsetWidth + "px";
+}
+
+item.forEach(link => {
+    link.addEventListener('click', (e) => {
+        indicator(e.target);
+    })
+})
+
